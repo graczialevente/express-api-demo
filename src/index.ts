ce3da3e8b92c68decc8642e854b9baafb5f22caa
Graczia Levente <graczia.levente@outlook.com>
2020-10-06 03:29:54 +0200

    chore: init project

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import Joi from 'joi';
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Gets current time
+ */
+app.get('/', (req, res) => {
+  res.send(new Date().toISOString());
+});
+
+/**
+ * Validate get parameter
+ */
+app.get('/post/:postId', (req, res) => {
+  const postId = parseInt(req.params.postId, 10);
+
+  if (isNaN(postId)) {
+    res.status(400).send('Not valid parameter.');
+    return;
+  }
+
+  res.send({ postId });
+});
+
+/**
+ * Validate posted object
+ */
+app.post('/post', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  res.send('OK');
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => console.log(`Listening on port: ${port}]`));
